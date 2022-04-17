441df6a7c491db1fe44797081e93158d98a9de22
BeLeap <beleap@beleap.codes>
2022-04-17 17:06:26 +0900

    :sparkles: implement skip whitespace

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Lexer<'a> {
     input: &'a str,
     position: usize,
@@ -28,20 +28,35 @@ impl <'a> Lexer<'a> {
             }
         }
     }
+
+    pub fn skip_whitespace(self) -> Self {
+        match self.ch {
+            ' ' | '\t' | '\n' | '\r' => self.read_char().skip_whitespace(),
+            _ => self,
+        }
+    }
 }
 
+#[cfg(test)]
 mod test {
     use super::Lexer;
 
     #[test]
     fn test_read_char() {
-        let content = String::from("adsf asdf asdf");
+        let content = String::from("Lorem Ipsum");
         let lexer = Lexer::new(&content);
-
-        println!("{:?}", lexer);
+        assert_eq!(lexer, Lexer { input: &content, position: 0, read_position: 1, ch: 'L' });
 
         let lexer = lexer.read_char();
+        assert_eq!(lexer, Lexer { input: &content, position: 1, read_position: 2, ch: 'o' });
+    }
 
-        println!("{:?}", lexer);
+    #[test]
+    fn test_skip_whitespace() {
+        let content = String::from(" \n \t \r Lorem Ipsum");
+        let lexer = Lexer::new(&content);
+
+        let lexer = lexer.skip_whitespace();
+        assert_eq!(lexer, Lexer { input: &content, position: 7, read_position: 8, ch: 'L' });
     }
 }
